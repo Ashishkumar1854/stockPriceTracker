4d18bfe9a7d8181c2b2bd3d07fd0120932959f79
Ashish Kumar <kashish84396@gmail.com>
2025-12-02 12:59:09 +0530

    check backend

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,11 @@
+cat > (src / routes / authRoutes.js) << "EOF";
+import express from "express";
+import { signup, login } from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+
+export default router;
+EOF;
