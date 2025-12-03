d60e8b6c0a0a3565b8f9859fb7102ded9b1ba77d
Ashish Kumar <kashish84396@gmail.com>
2025-12-03 20:35:39 +0530

    redis

---
+++ backend/src/utils/redisClient.js
@@ -0,0 +1,11 @@
+// backend/src/utils/redisClient.js
+import Redis from "ioredis";
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
+  // if password needed:
+  // password: process.env.REDIS_PASSWORD || undefined,
+});
+
+export default redis;
